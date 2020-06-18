b7c81b291c7999a28d358465614f4298a73b8e00
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-06-17 21:24:57 -0400

    playstore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res
@@ -20,76 +21,144 @@ app.get('/echo', (req, res) => {
     res.send(responseText);
   });
 
-  app.get('/quotient', (req, res) => {
-    const { a, b } = req.query;
-  
-    if (!a) {
-      return res
-        .status(400)
-        .send('Value for a is needed');
+  app.get('/playstore', (req, res) => {
+    const { search = " ", sort } = req.query;
+
+    if (sort) {
+      if (!['title', 'rank'].includes(sort)) {
+        return res
+          .status(400)
+          .send('Sort must be one of title or rank');
+      }
     }
   
-    if (!b) {
-      return res
-        .status(400)
-        .send('Value for b is needed');
+    let results = App
+          .filter(app =>
+              app
+                .title
+                .toLowerCase()
+                .includes(search.toLowerCase()));
+  
+    if (sort) {
+      results
+        .sort((a, b) => {
+          return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
     }
-  
-    const numA = parseFloat(a);
-    const numB = parseFloat(b);
-  
-    if (isNaN(numA)) {
-      return res
-        .status(400)
-        .send('Value for a must be numeric');
-    }
-  
-    if (isNaN(numB)) {
-      return res
-        .status(400)
-        .send('Value for b must be numeric');
-    }
-  
-    if (numB == 0) {
-      return res
-        .status(400)
-        .send('Cannot divide by 0');
-    }
-  
-    const ans = numA / numB;
   
     res
-      .send(`${a} divided by ${b} is ${ans}`);
+      .json(results);
   });
 
-  app.get('/generate', (req, res) => {
-    // get n from the query string in the request
-    const { n } = req.query;
+
+  // app.get('/quotient', (req, res) => {
+  //   const { a, b } = req.query;
   
-    // coerce n to a numeric value
-    const num = parseInt(n);
+  //   if (!a) {
+  //     return res
+  //       .status(400)
+  //       .send('Value for a is needed');
+  //   }
   
-    if (isNaN(num)) {
-      return res
-       .status(400)
-       .send('Invalid request');
-    }
+  //   if (!b) {
+  //     return res
+  //       .status(400)
+  //       .send('Value for b is needed');
+  //   }
   
-    // generate array [1..n]
-    const initial =  Array(num)
-      .fill(1)
-      .map((_, i) => i + 1);
+  //   const numA = parseFloat(a);
+  //   const numB = parseFloat(b);
   
-    // shuffle the array
-    initial.forEach((e, i) => {
-      let ran = Math.floor(Math.random() * num);
-      let temp = initial[i];
-      initial[i] = initial[ran];
-      initial[ran] = temp;
-    })
+  //   if (isNaN(numA)) {
+  //     return res
+  //       .status(400)
+  //       .send('Value for a must be numeric');
+  //   }
   
-    res.json(initial);
-  });
+  //   if (isNaN(numB)) {
+  //     return res
+  //       .status(400)
+  //       .send('Value for b must be numeric');
+  //   }
+  
+  //   if (numB == 0) {
+  //     return res
+  //       .status(400)
+  //       .send('Cannot divide by 0');
+  //   }
+  
+  //   const ans = numA / numB;
+  
+  //   res
+  //     .send(`${a} divided by ${b} is ${ans}`);
+  // });
+
+  // app.get('/generate', (req, res) => {
+  //   // get n from the query string in the request
+  //   const { n } = req.query;
+  
+  //   // coerce n to a numeric value
+  //   const num = parseInt(n);
+  
+  //   if (isNaN(num)) {
+  //     return res
+  //      .status(400)
+  //      .send('Invalid request');
+  //   }
+  
+  //   // generate array [1..n]
+  //   const initial =  Array(num)
+  //     .fill(1)
+  //     .map((_, i) => i + 1);
+  
+  //   // shuffle the array
+  //   initial.forEach((e, i) => {
+  //     let ran = Math.floor(Math.random() * num);
+  //     let temp = initial[i];
+  //     initial[i] = initial[ran];
+  //     initial[ran] = temp;
+  //   })
+  
+  //   res.json(initial);
+  // });
+
+  // function toRadians(deg) {
+  //   return deg * (Math.PI / 180);
+  // }
+  
+  // function toDegrees(rad) {
+  //   return rad * (180 / Math.PI);
+  // }
+  
+  // app.get('/midpoint', (req, res) => {
+  //   const { lat1, lon1, lat2, lon2 } = req.query;
+  
+  //   // for brevity the validation is skipped
+  
+  //   // convert to radians
+  //   const rlat1 = toRadians(lat1);
+  //   const rlon1 = toRadians(lon1);
+  //   const rlat2 = toRadians(lat2);
+  //   const rlon2 = toRadians(lon2);
+  
+  //   const bx = Math.cos(rlat2) * Math.cos(rlon2 - rlon1);
+  //   const by = Math.cos(rlat2) * Math.sin(rlon2 - rlon1);
+  
+  //   const midLat = Math.atan2(
+  //     Math.sin(rlat1) + Math.sin(rlat2),
+  //     Math.sqrt(
+  //       (Math.cos(rlat1) + bx)
+  //       * (Math.cos(rlat1) + bx)
+  //       + by * by
+  //     )
+  //   );
+  //   const midLon = rlon1 + Math.atan2(by, Math.cos(rlat1) + bx);
+  
+  //   res.json({
+  //     lat: toDegrees(midLat),
+  //     lon: toDegrees(midLon)
+  //   })
+  // });
 
 
   module.exports = app;
